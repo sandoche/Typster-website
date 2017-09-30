cf64c8acce492c8425440636cdbb281449329db8
Sandoche Adittane <sandoche@gmail.com>
2017-09-30 18:04:09 +0200

    animations added

--- _src/index.js
+++ _src/index.js
@@ -1,2 +1,12 @@
+// JS umports
+import ScrollReveal from 'scrollreveal';
+
 // CSS and SASS files
 import './index.scss';
+
+
+window.sr = ScrollReveal({ viewOffset: { top: 0, right: 0, bottom: 100, left: 0 }, delay: 300 });
+
+sr.reveal('.feature__row');
+sr.reveal('.product', 100);
+sr.reveal('.newsletter');
