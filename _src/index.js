9fe0949b0d29d38548ac34ea54f31fa906b48393
Sandoche Adittane <sandoche@gmail.com>
2017-10-03 18:29:33 +0200

    smooth scroll added

--- _src/index.js
+++ _src/index.js
@@ -1,9 +1,11 @@
 // JS umports
 import ScrollReveal from 'scrollreveal';
+import SmoothScroll from 'smooth-scroll';
 
 // CSS and SASS files
 import './index.scss';
 
+const scroll = new SmoothScroll('a[href*="#"]');
 
 window.sr = ScrollReveal({ viewOffset: { top: 0, right: 0, bottom: 100, left: 0 }, delay: 300 });
 
